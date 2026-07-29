624e535b5e13fe0038fb5f26b251accfe9a34d60
greyemine <greyemine@gmail.com>
2026-07-29 17:44:26 +0300

    Orfo edit "перенос строки"

--- pythonProject1/bot.py
+++ pythonProject1/bot.py
@@ -281,9 +281,10 @@ async def add_bottom_and_buttons_from_text(message: types.Message,
     print('from_user_first_name_value -', from_user_first_name_value)
 
     builder, origin_message_id,local_user_ratings_dic = await technical_processing_form(from_user_id_value,
-                                                                 message,
-                                                                 origin_message_id_value,
-                                                                 local_user_ratings_dic)
+                                                                                        message,
+                                                                                        origin_message_id_value,
+                                                                                        local_user_ratings_dic
+                                                                                        )
 
     like_value: int = dict_of_form_evaluation[origin_message_id]["like"]
     super_like_value: int = dict_of_form_evaluation[origin_message_id]["super_like"]
