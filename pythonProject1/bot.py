3d4db5f5cf997fce74948f8ce76900208d8856fa
greyemine <greyemine@gmail.com>
2026-07-29 17:42:23 +0300

    Orfo edit "перенос строки"

--- pythonProject1/bot.py
+++ pythonProject1/bot.py
@@ -241,7 +241,10 @@ async def add_bottom_and_buttons_def_from_photo(message: types.Message,
                                                 origin_message_id_value: str = None
                                                 ):
 
-    builder, origin_message_id = await technical_processing_form(from_user_id_value, message, origin_message_id_value)
+    builder, origin_message_id = await technical_processing_form(from_user_id_value,
+                                                                 message,
+                                                                 origin_message_id_value
+                                                                 )
 
     print('We are handling photo')
     print('Position _ ', message.caption.find('_________________'))
